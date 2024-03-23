f1b5a478017fb53a42f056df0ea2ff87fece0509
Polina <polinabondar4@gmail.com>
2024-03-23 12:41:15 +0200

    make build

---
+++ index.3fcef94c.js
@@ -0,0 +1,2 @@
+var c=document.querySelector(".icon--menu"),e=document.querySelector(".icon--close"),o=document.body;c.addEventListener("click",function(){o.classList.add("lock-scroll")}),e.addEventListener("click",function(){o.classList.remove("lock-scroll")});
+//# sourceMappingURL=index.3fcef94c.js.map
